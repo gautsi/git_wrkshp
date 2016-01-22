a5f6755e5cd58d1c56384425c768e7e6677edb73
Gautam Sisodia <gautamsisodia@KP-macbook.local>
2016-01-22 07:12:44 -0500

    Added doctest for func

--- git_wrkshp/git_wrkshp.py
+++ git_wrkshp/git_wrkshp.py
@@ -6,10 +6,14 @@ import numpy as np
 def add(a, b):
     '''
     >>> add(2, 3)
-    5
+    6
     '''
     return func(a, b)
 
 
 def func(a, b):
+    '''
+    >>> func(2, 3)
+    6
+    '''
     return np.prod([a, b])
